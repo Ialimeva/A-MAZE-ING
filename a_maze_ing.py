15f00f3ed2eb3d68438e7e7bbe5c05a5e4992f96
Teddy Andrianina Rakotozafy <trakotoz@student.42antananarivo.mg>
2026-04-21 17:56:47 +0300

    [Main program]: Adding execution format + configuration read

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -1,1 +1,27 @@
 #!/usr/bin/env python3
+
+import sys
+from core import ConfigManager
+
+
+def main() -> None:
+    if len(sys.argv) != 2:
+        print(
+            "Invalid execution format - try:\n"
+            f"python3 a_maze_ing.py <config_file>"
+        )
+        sys.exit(1)
+
+    config: ConfigManager = ConfigManager(sys.argv[1])
+
+    all_configuration: dict[
+        str,
+        str | bool | int | tuple[int, int]
+    ] = config.get_config()
+
+    print(all_configuration)
+
+
+if __name__ == "__main__":
+    print("=== A-Maze-ing ===\n")
+    main()
