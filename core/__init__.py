1aef55dd326ef2570232cd9cfa189959dbc3498a
Teddy Andrianina Rakotozafy <trakotoz@student.42antananarivo.mg>
2026-04-25 00:54:18 +0300

    [Core]: New structure for config and solver

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,4 @@
-from .read_config import ConfigManager
-from .maze_exporter import MazeExporter
-from .forty_two_pattern import Pattern42
-
+from .maze_config import ConfigManager, MazeExporter, Pattern42
 
 __all__ = [
     "ConfigManager",
