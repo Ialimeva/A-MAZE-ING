7477f1c044abc32adf16b0faaec55ac199f59bc9
Teddy Andrianina Rakotozafy <trakotoz@student.42antananarivo.mg>
2026-05-01 15:24:44 +0300

    [Maze Exporter]: exporter having two utilites: export to file and export in format str hexadecimal
    MazeExporter class change to a full class method where the value is
    passed by argument. export_maze() just give the format str hexadecimal
    list[list[str]] and export_maze_file() export to the given filename

--- core/maze_config/maze_exporter.py
+++ core/maze_config/maze_exporter.py
@@ -6,58 +6,51 @@ class MazeExporterError(Exception):
 
 
 class MazeExporter:
-    def __init__(
-        self,
-        entry: tuple[int, int],
-        exit_: tuple[int, int],
-        grid: Optional[list[list[int]]] = None,
+
+    @classmethod
+    def export_maze_file(
+        cls,
+        filename: str,
+        grid: list[list[int]],
+        entry_point: Optional[tuple[int, int]],
+        exit_point: Optional[tuple[int, int]]
     ) -> None:
-
-        self.__grid: list[list[int]] = []
-        if grid:
-            self.__grid = grid
-
-        self.entry: tuple[int, int] = entry
-        self.exit: tuple[int, int] = exit_
-
-    def set_grid(self, grid: list[list[int]]) -> None:
-        if not grid:
-            raise MazeExporterError("Invalid Grid")
-        self.__grid = grid
-
-    def export_maze_file(self, filename: str) -> None:
         try:
             if not filename:
                 raise MazeExporterError("Invalid file")
             with open(filename, "w") as f:
-                res: list[list[str]] = self.__parse_grid()
+                res: list[list[str]] = cls.__parse_grid(grid)
                 output: str = "\n".join("".join(row) for row in res)
                 f.write(output)
-
                 f.write("\n")
-                f.write(str(self.entry[0]) + "," + str(self.entry[1]) + "\n")
-                f.write(str(self.exit[0]) + "," + str(self.exit[1]) + "\n")
+
+                if (entry_point and exit_point):
+                    f.write("\n")
+                    f.write(str(entry_point[0]) + "," + str(entry_point[1]) + "\n")
+                    f.write(str(exit_point[0]) + "," + str(exit_point[1]) + "\n")
 
         except Exception as e:
             raise MazeExporterError(f"Export failed: {e}")
 
-    def export_maze(self) -> list[list[str]]:
-        return self.__parse_grid()
+    @classmethod
+    def export_maze(cls, grid: list[list[int]]) -> list[list[str]]:
+        return cls.__parse_grid(grid)
 
-    def __parse_grid(self) -> list[list[str]]:
-        if not self.__grid:
+    @classmethod
+    def __parse_grid(cls, grid: list[list[int]]) -> list[list[str]]:
+        if not grid:
             raise MazeExporterError("Empty grid")
         output: list[list[str]] = []
-        height: int = len(self.__grid)
-        width: int = len(self.__grid[0])
+        height: int = len(grid)
+        width: int = len(grid[0])
 
         for i in range(1, height - 1, 2):
             row: list[str] = []
             for j in range(1, width - 1, 2):
-                west = self.__grid[i][j - 1]
-                south = self.__grid[i + 1][j]
-                east = self.__grid[i][j + 1]
-                north = self.__grid[i - 1][j]
+                west = grid[i][j - 1]
+                south = grid[i + 1][j]
+                east = grid[i][j + 1]
+                north = grid[i - 1][j]
 
                 value = (
                     (north << 0) |
