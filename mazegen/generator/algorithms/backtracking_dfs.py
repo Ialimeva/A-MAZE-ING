d45d3345b3038091439aa1be7d8b65e4c6bf61ac
Teddy Andrianina Rakotozafy <trakotoz@student.42antananarivo.mg>
2026-05-19 07:28:54 +0300

    [Maze Generator]: Polish of redendunt function behavior

--- mazegen/generator/algorithms/backtracking_dfs.py
+++ mazegen/generator/algorithms/backtracking_dfs.py
@@ -1,4 +1,4 @@
-from ..generator_base import GeneratorError, MazeGenerator
+from ..generator_base import MazeGenerator
 from typing import Generator
 from mazegen.maze import Maze
 from mazegen.maze_config import MazeConfig
@@ -14,64 +14,7 @@ class GeneratorDFS(MazeGenerator):
         super().__init__(configs)
         self.__visited: set[tuple[int, int]] = set()
 
-    def generate(self) -> Maze:
-        self.__visited.clear()
-        if not self.__carve(1, 1):
-            raise GeneratorError("Error on generating the Maze")
-        return super().generate()
-
     def __carve(
-        self,
-        start_x: int,
-        start_y: int
-    ) -> bool:
-        self.__visited.clear()
-
-        stack: list[tuple[int, int]] = [(start_x, start_y)]
-        self.__visited.add((start_x, start_y))
-        self._maze.grid[start_y][start_x] = 0
-
-        while stack:
-            pos_x, pos_y = stack[-1]
-
-            directions: list[tuple[int, int]] = [
-                (2, 0),
-                (-2, 0),
-                (0, -2),
-                (0, 2)
-            ]
-            self._random.shuffle(directions)
-
-            pushed: bool = False
-            for dx, dy in directions:
-                npos_x, npos_y = dx + pos_x, dy + pos_y
-
-                if (
-                    self.is_valid_pos(npos_x, npos_y) and
-                    (npos_x, npos_y) not in self.__visited
-                ):
-                    self.__visited.add((npos_x, npos_y))
-                    stack.append((npos_x, npos_y))
-
-                    self._maze.grid[(dy // 2) + pos_y][(dx // 2) + pos_x] = 0
-                    self._maze.grid[npos_y][npos_x] = 0
-
-                    pushed = True
-                    break
-
-            if not pushed:
-                stack.pop()
-
-        return True
-
-    def generate_step(self) -> Generator[Maze, None, None]:
-        self.__visited.clear()
-
-        yield self._maze
-        yield from self.__carve_step(1, 1)
-        yield from super().generate_step()
-
-    def __carve_step(
         self,
         start_x: int,
         start_y: int
@@ -116,3 +59,19 @@ class GeneratorDFS(MazeGenerator):
                 stack.pop()
 
         yield self._maze
+
+    def generate(self) -> Maze:
+        self.__visited.clear()
+        gen = self.__carve(1, 1)
+
+        for val in gen:
+            self._maze = val
+        return super().generate()
+
+    def generate_step(self) -> Generator[Maze, None, None]:
+        self.__visited.clear()
+        gen = self.__carve(1, 1)
+
+        yield self._maze
+        yield from gen
+        yield from super().generate_step()
