9e52a678aef1214846aef0e2b705c4013fdd299c
Teddy Andrianina Rakotozafy <trakotoz@student.42antananarivo.mg>
2026-05-23 11:29:40 +0300

    [A-maze-ing]: introduction on main program

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -3,9 +3,10 @@
 import sys
 from typing import Any
 from core import ConfigManager
-from mazegen import GeneratorRegistry, SolverRegistry, Maze
+from mazegen import GeneratorRegistry, SolverRegistry
 from cli_render import VisualTerm
 from display import Game, DisplayConfig
+import time
 
 
 def usage_exit() -> None:
@@ -13,6 +14,24 @@ def usage_exit() -> None:
     print("Usage: \033[34mpython a_maze_ing.py <file config>\033[0m")
     print("Please try again :)")
     sys.exit(1)
+
+
+def introduction():
+    a_maze_ing: str = r"""
+ █████╗       ███╗   ███╗ █████╗ ███████╗███████╗      ██╗███╗   ██╗ ██████╗
+██╔══██╗      ████╗ ████║██╔══██╗╚══███╔╝██╔════╝      ██║████╗  ██║██╔════╝
+███████║█████╗██╔████╔██║███████║  ███╔╝ █████╗  █████╗██║██╔██╗ ██║██║  ███╗
+██╔══██║╚════╝██║╚██╔╝██║██╔══██║ ███╔╝  ██╔══╝  ╚════╝██║██║╚██╗██║██║   ██║
+██║  ██║      ██║ ╚═╝ ██║██║  ██║███████╗███████╗      ██║██║ ╚████║╚██████╔╝
+╚═╝  ╚═╝      ╚═╝     ╚═╝╚═╝  ╚═╝╚══════╝╚══════╝      ╚═╝╚═╝  ╚═══╝ ╚═════╝
+    """
+    print("\033[H\033[J")
+    output: list[str] = str(a_maze_ing).split("\n")
+    for line in output:
+        print(line)
+        time.sleep(0.1)
+    print("Loading ...")
+    time.sleep(0.1)
 
 
 def get_config(filename: str) -> dict[str, Any]:
@@ -23,13 +42,32 @@ def get_config(filename: str) -> dict[str, Any]:
     ).get_config()
 
 
+def print_config(configs: dict[str, Any]) -> None:
+    output: str = "=== Configuration ===\n"
+
+    output += "WIDTH: " + f"{configs['width']}\n"
+    output += "HEIGHT: " + f"{configs['height']}\n"
+    output += "ENTRY: " + f"{configs['entry']}\n"
+    output += "EXIT: " + f"{configs['exit']}\n"
+    output += "OUTPUT FILE: " + f"{configs['output_file']}\n"
+    output += "PERFECT: " + f"{configs['perfect']}\n"
+    output += "SEED: " + f"{configs['seed']}\n"
+    output += "GENERATOR: " + f"{configs['generator']}\n"
+    output += "SOLVER: " + f"{configs['solver']}\n"
+    output += "VISUAL: " + f"{configs['visual']}\n"
+    output += "====================="
+
+    print(output)
+
 def main() -> None:
     if len(sys.argv) != 2:
         usage_exit()
 
+    introduction()
+
     configs: dict[str, Any] = get_config(sys.argv[1])
-    print(configs)
-    print("\033[H\033[J")
+    print_config(configs)
+
     if configs["visual"] == "term":
         term_render: VisualTerm = VisualTerm(configs)
         term_render.run()
